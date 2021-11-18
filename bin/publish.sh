84287f93f197abac1a14b095a2742421809f3674
etherio <etherio97@users.noreply.github.com>
2021-11-19 00:36:49 +0630

    [publish:patch]

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-version=$(cat package.json|grep version|cut -d ' ' -f 4)
+version=$(cat package.json|grep version|cut -d ' ' -f 4|cut -d '"' -f 2)
 mode=${1:-'patch'}
 
 echo
 
 sleep 1
-echo ">> Deleting v${version:-'0.0.0'}"
+echo ">> Deleting v${version:?'required semver'}"
 yarn clean
 
 sleep 1
 
-next_version=$(npx semver ${version:-'0.0.0'} -i $mode)
-echo ">> changing semantic version ${version:-'0.0.0'} to ${next_version}"
+next_version=$(npx semver $version -i $mode)
+echo ">> changing semantic version $version to ${next_version}"
 
 sleep 1
 
