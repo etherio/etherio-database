2c5d564bf608bfa6d0c8effbef2d13774f027a2a
Shin Maung Maung <shinmaungmaung@mytel.com.mm>
2021-11-19 14:50:51 +0630

    [publish:patch]

--- dist/src/lib/Document.js
+++ dist/src/lib/Document.js
@@ -1,24 +1,14 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Document = void 0;
+var tslib_1 = require("tslib");
 var Document = (function () {
     function Document(_id, data) {
         this._id = _id;
         this.data = data;
     }
     Document.prototype.toJSON = function () {
-        return __assign({ _id: this._id }, this.data);
+        return (0, tslib_1.__assign)({ _id: this._id }, this.data);
     };
     return Document;
 }());
